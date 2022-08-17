c3e264ecc2d30b4aa15f14ca62c20cf35c824be1
mercylynn <mercy.kamene@student.moringaschool.com>
2022-08-17 06:09:40 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+
+{
+console.log( multiply);
+}
+ 
+let random=Math.floor(Math.random() * 10) + 1;
+
+
+let num3 =70;
+let num4=11;
+let mod=num3 % num4;
+{
+console.log (mod);}
+
+let max=Math.max(20,10,5);
